82be26389afa1d4fc132b9c2eff3a75c0bacc5e5
NiftyAndy <andy@niftyleague.com>
2023-04-12 17:42:36 -0400

    Styles cleanup

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -1,32 +1,33 @@
 import Auth from '@/components/Auth';
 
+import styles from '@/styles/profile.module.css';
+
 export default function Inventory() {
   const { currencies, isLoggedIn } = Auth.useUser();
   return isLoggedIn ? (
     <>
       <label htmlFor="inventory">Currencies</label>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-        }}
-      >
+      <div className={styles.currencyContainer}>
         <input
           id="T1"
-          style={{ width: '45%' }}
+          className={styles.currency}
           type="text"
-          value={`Brawl Bucks: ${currencies?.T1 || 0}`}
+          value={`Brawl Bucks:  ${currencies?.T1 || 0} `}
           disabled
         />
         <input
           id="T2"
-          style={{ width: '45%' }}
+          className={styles.currency}
           type="text"
-          value={`Nifty Nuggets: ${currencies?.T2 || 0}`}
+          value={`Nifty Nuggets:  ${currencies?.T2 || 0} `}
           disabled
         />
       </div>
+      <hr className={styles.hr} />
       <label htmlFor="characters">Characters</label>
+      <div>coming soon...</div>
+      <hr className={styles.hr} />
+      <label htmlFor="characters">Items</label>
       <div>coming soon...</div>
     </>
   ) : null;

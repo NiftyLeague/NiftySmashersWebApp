6dda855127b8ea7de47bd5075461a7007a911ff1
NiftyAndy <andy@niftyleague.com>
2023-05-04 17:26:28 -0400

    Add additional sections to homepage from main site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Navbar from '@/components/Navbar';
+import ConsoleGame from '@/components/ConsoleGame';
+import DegensSection from '@/components/DegensSection';
+import Footer from '@/components/Footer';
 import TrailerModal from '@/components/TrailerModal';
 import GameSelectModal from '@/components/GameSelectModal';
 import UnityModal from '@/components/UnityModal';
@@ -16,8 +19,26 @@ export default function Home() {
       <Head>
         <title>Nifty Smashers</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          property="og:title"
+          content="Nifty Smashers - Free to Play Fighting Game"
+        />
+        <meta
+          name="description"
+          content="Nifty Smashers is a free 3D platform fighting game that supports up to 16 local or online players. Available on all mobile devices, full cross-play coming soon!"
+          key="desc"
+        />
+        <meta
+          property="og:description"
+          content="Nifty Smashers is a free 3D platform fighting game that supports up to 16 local or online players. Available on all mobile devices, full cross-play coming soon!"
+        />
+        <meta
+          property="og:image"
+          content="https://niftysmashers.com/assets/console-game/classic-gaming-reinvented.png"
+        />
       </Head>
       <section className={styles.main}>
+        <div className="radial-gradient-background-header" />
         <div className={styles.container}>
           <Navbar />
           <div className={styles.content}>
@@ -52,6 +73,13 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section className={styles.console_game}>
+        <ConsoleGame src="/assets/smashers.mp4" />
+      </section>
+      <section className={styles.game_details}>
+        <DegensSection />
+      </section>
+      <Footer classes={{ footer: styles.footer }} />
       <TrailerModal />
       <GameSelectModal launchGame={launchGame} />
       <UnityModal gameOpen={gameOpen} closeGame={closeGame} />
